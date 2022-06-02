b830e41261cba58c72e2c59fab1a9cb38d56256b
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-06-02 23:34:00 +0600

    add details

--- Components/Country.js
+++ Components/Country.js
@@ -5,7 +5,11 @@ export default function Country({ country }) {
   return (
     <View style={styles.items}>
       <Text style={styles.title}>Name: {country.name?.common}</Text>
-      <Text style={styles.title}>Capital: {country.capital}</Text>
+      <Text style={styles.title}>Capital: {country?.capital}</Text>
+      <Text style={styles.title}>Area: {country?.area}</Text>
+      <Text style={styles.title}>Population: {country?.population}</Text>
+      <Text style={styles.title}>region: {country?.region}</Text>
+      {/* <Text style={styles.title}>Language: {country?.language}</Text> */}
       <Image
         source={{
           uri: country?.flags?.png,
